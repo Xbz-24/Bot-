00015708bec490ed58fb8385f3b00327462c2a35
daily.dev <daily@waify.club>
2024-01-09 18:01:58 -0300

    made this nix derivation more versatile

--- default.nix
+++ default.nix
@@ -1,46 +1,22 @@
-{ 
-   stdenv 
-   ,lib 
-   ,fetchurl 
-   ,autoPatchelfHook 
-   ,openssl
-   ,zlib
-   ,libsodium
-   ,opus
-   ,gcc
-}:
-
-stdenv.mkDerivation rec {
+{ pkgs }:
+pkgs.stdenv.mkDerivation rec {
   pname = "dpp";
   version = "10.0.29";
 
-  src = fetchurl {
-    url = "https://github.com/brainboxdotcc/DPP/releases/download/v${version}/libdpp-${version}-linux-x64.deb";
-    sha256 = "11ssfmba0cdipls2f2fgsb8dp6fbx3mkk6y18kzgzcdhsvh9mgk8";
+  src = pkgs.fetchFromGitHub {
+    owner = "brainboxdotcc";
+    repo = "DPP";
+    rev = "v${version}";
+    sha256 = "BJMg3MLSfb9x/2lPHITeI3SWwW1OZVUUMVltTWUcw9I=";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook ];
+  nativeBuildInputs = with pkgs; [ cmake autoPatchelfHook libopus];
+  buildInputs = with pkgs; [ openssl zlib libsodium gcc ];
 
-  unpackPhase = ''
-    ar x $src
-    tar xvf data.tar.gz
-  '';
-
-  installPhase = ''
-    mkdir -p $out
-    ls -l ${libsodium}/lib
-    cp -R usr/* $out/
-  '';
-
-
-  postFixup = ''
-    patchelf --set-rpath $ORIGIN/../${lib.makeLibraryPath [ openssl zlib libsodium opus gcc ]} $out/lib/libdpp.so.10.0.29
-    patchelf --replace-needed libsodium.so.23 libsodium.so.26 $out/lib/libdpp.so.10.0.29
-  '';
-
-  meta = with lib; {
+  meta = with pkgs.lib; {
     description = "D++ Discord C++ Library";
     homepage = "https://github.com/brainboxdotcc/DPP";
-    license = licenses.asl20; 
+    license = pkgs.lib.licenses.asl20;
+    maintainers = with pkgs.lib.maintainers; [ renato ];
   };
 }
--- shell.nix
+++ shell.nix
@@ -1,15 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
-
+let
+   dpp = import ./default.nix { inherit pkgs; };
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [ 
-	  (pkgs.callPackage ~/Nix-Stuff/DPP/default.nix {
-		openssl = openssl.out;
-		zlib = zlib.out;
-		libsodium = libsodium.out;
-		opus = libopus.out;
-		gcc = gcc-unwrapped.lib;
-	  })
-	gcc
 	cmake
+	dpp
+	openssl
+	zlib
+	libsodium
+	libopus
+	pkg-config
   ];
 }
