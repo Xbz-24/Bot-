5c043287f47f827216718eaad35b8dc17ef6fb86
daily.dev <daily@waify.club>
2024-01-09 18:15:39 -0300

    added reading token from file for not leaking my token lol

---
+++ bot.cpp
@@ -0,0 +1,46 @@
+#include <dpp/dpp.h>
+#include <fstream>
+#include <string>
+#include <iostream>
+
+std::string get_token_from_file(const std::string& filename){
+    std::ifstream stream(filename);
+    std::string token;
+    if(stream){
+        std::getline(stream, token);
+        size_t pos = token.find('=');
+        if(pos != std::string::npos){
+            token = token.substr(pos + 1);
+        }
+    }else{
+        std::cerr << "Unable to open config file!\n";
+    }
+    return token;
+}
+
+int main() {
+
+    std::string token = get_token_from_file("../token.txt");
+    if(token.empty()){
+        std::cerr << "Token is empty. Ensure the config file is set up correctly.\n";
+        return 1;
+    }
+
+    dpp::cluster bot(token);
+
+    bot.on_log(dpp::utility::cout_logger());
+
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+        if (event.command.get_command_name() == "ping") {
+            event.reply("Pong!");
+        }
+    });
+
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        if (dpp::run_once<struct register_bot_commands>()) {
+            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+        }
+    });
+
+    bot.start(dpp::st_wait);
+}
